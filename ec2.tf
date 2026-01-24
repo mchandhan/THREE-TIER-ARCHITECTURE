96eb6dd27830b661975e6a59094433a3d54670aa
rashmigmr13-eng <rashmigmr13@gmail.com>
2026-01-24 21:29:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_1.id
   security_groups = [aws_security_group.web_sg.id]
 
